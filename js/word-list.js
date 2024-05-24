28f5867b8323ff7edef07a17def433c4c97ca023
João <camposjoao177@gmail.com>
2024-05-24 10:48:38 -0300

    Add files via upload

---
+++ js/word-list.js
@@ -0,0 +1,32 @@
+const wordList = [
+    {
+        word: "computador",
+    },
+    {
+        word: "musica",
+    },
+    {
+        word: "geladeira",
+    },
+    {
+        word: "chuteira",
+    },
+    {
+        word: "ventilador",
+    },
+    {
+        word: "internet",
+    },
+    {
+        word: "macaco",
+    },
+    {
+        word: "jogo",
+    },
+    {
+        word: "pequeno",
+    },
+    {
+        word: "palavra",
+    },
+];
---
+++ js/words.js
@@ -0,0 +1,42 @@
+let words = [
+    {
+        word: "computador",
+        hint: "Máquina destinada ao processamento de dados"
+    },
+    {
+        word: "musica",
+        hint: "Combinação harmoniosa de sons"
+    },
+    {
+        word: "geladeira",
+        hint: "Aparelho constituído por um móvel termicamente isolado"
+    },
+    {
+        word: "chuteira",
+        hint: "Sapato fechado por tiras ou cadarços e dotado de travas de couro"
+    },
+    {
+        word: "ventilador",
+        hint: "Que ventila"
+    },
+    {
+        word: "internet",
+        hint: "rede de computadores dispersos por todo o planeta"
+    },
+    {
+        word: "macaco",
+        hint: "Símio"
+    },
+    {
+        word: "jogo",
+        hint: "Atividade recreativa que tem por finalidade entreter"
+    },
+    {
+        word: "pequeno",
+        hint: "De reduzido porte"
+    },
+    {
+        word: "palavra",
+        hint: "Termo"
+    },
+]
